696ae751ad5cc10ef5f239f2dd38c40602995730
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2021-02-24 13:13:04 +0100

    Add sms/reply function

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,5 +1,5 @@
 exports.handler = function(context, event, callback) {
-  const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say('Hello World!');
+  const twiml = new Twilio.twiml.MessagingResponse();
+  twiml.message(`hello world`);
   callback(null, twiml);
 };
--- functions/sms/reply.protected.js
+++ functions/sms/reply.protected.js
@@ -1,5 +1,32 @@
+const request = require('request-promise-native')
+
 exports.handler = function(context, event, callback) {
+  const { ACCOUNT_SID, AUTH_TOKEN, SEND_SMS_FROM, SEND_SMS_TO } = process.env;
+
   const twiml = new Twilio.twiml.MessagingResponse();
-  twiml.message('Hello World!');
+  // twiml.message(`Source number: ${SEND_SMS_FROM}`);
+
+  request.post({
+    url: `https://api.twilio.com/2010-04-01/Accounts/${ACCOUNT_SID}/Messages.json`,
+    json: true,
+    'auth': {
+      'user': ACCOUNT_SID,
+      'pass': AUTH_TOKEN
+    },
+    form: {
+      From: SEND_SMS_FROM,
+      To: SEND_SMS_TO,
+      Body: `Remember about dinner`
+    }
+  })
+  .then((data) => {
+    console.log(`Message successfully sent to ${SEND_SMS_TO}`);
+    callback(null, true);
+  })
+  .catch((err) => {
+    console.log(err);
+    return callback(err);
+  });
+
   callback(null, twiml);
 };
